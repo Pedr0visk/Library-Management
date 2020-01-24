4f46d92221ac53b7f731d5ad1ee2d1540df56665
pbelloto <pbelloto1997@gmail.com>
2020-01-24 08:02:47 -0300

    attribute name changed

--- app/Book.php
+++ app/Book.php
@@ -6,46 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $guarded = [];
+   protected $guarded = [];
 
-    public function path()
-    {
-        return '/books/' . $this->id;
-    }
+   public function path()
+   {
+      return '/books/' . $this->id;
+   }
 
-    public function setAuthorIdAttribute($author)
-    {
-        $this->attributes['author_id'] = Author::firstOrCreate([
-            'name' => $author
-        ])->id;
-    }
-    
-    public function checkout(User $user)
-    {
-        $this->reservations()->create([
-            'user_id' => $user->id,
-            'checked_out_at' => now(),
-        ]);
-    }
+   public function setAuthorIdAttribute($author)
+   {
+      $this->attributes['author_id'] = Author::firstOrCreate([
+         'name' => $author
+      ])->id;
+   }
+   
+   public function checkout(User $user)
+   {
+      $this->reservations()->create([
+         'user_id' => $user->id,
+         'checked_out_at' => now(),
+      ]);
+   }
 
-    public function checkin(User $user)
-    {   
-        $reservation = $this->reservations()->where('user_id', $user->id)
-            ->whereNotNull('checkout_out_at')
-            ->whereNull('checked_in_at')
-            ->first();
+   public function checkin(User $user)
+   {   
+      $reservation = $this->reservations()->where('user_id', $user->id)
+         ->whereNotNull('checkout_out_at')
+         ->whereNull('checked_in_at')
+         ->first();
 
-        if (is_null($reservation)) {
-            throw new \Exception();
-        }
+      if (is_null($reservation)) {
+         throw new \Exception();
+      }
 
-        $reservation->update([
-            'checked_in_at' => now(),
-        ]);
-    }
+      $reservation->update([
+         'checked_in_at' => now(),
+      ]);
+   }
 
-    public function reservations()
-    {
-        return $this->hasMany(Reservation::class);
-    }
+   public function reservations()
+   {
+      return $this->hasMany(Reservation::class);
+   }
 }
